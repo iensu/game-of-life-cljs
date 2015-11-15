c644254f28a1058a8776df0f5e160c95314b30d6
Jens Östlund <jostlund@gmail.com>
2015-11-15 21:23:24 +0100

    Cleaned up code.
    - Cells are only represented as either `:dead` or `:alive` in the grid

--- src/gol_cljs/core.cljs
+++ src/gol_cljs/core.cljs
@@ -2,34 +2,31 @@
 
 (enable-console-print!)
 
-(defn create-cell
-  [state x y]
-  {:state state :position [x y]})
-
 (defn within-bounds?
-  [grid x y]
+  [grid [x y]]
   (and (< 0 x (count grid))
        (< 0 y (count (nth grid 0)))))
 
 (defn get-cell
-  [grid x y]
-  (if (within-bounds? grid x y)
+  [grid [x y]]
+  (if (within-bounds? grid [x y])
     (-> grid
         (nth x)
         (nth y))
     nil))
 
 (defn update-cell
-  [grid {:keys [position] :as cell}]
+  [grid position cell]
   (let [[x y] position]
-    (if (within-bounds? grid x y)
-      (update-in grid [x y] (fn [_] cell))
+    (if (within-bounds? grid position)
+      (update-in grid position (fn [_] cell))
       grid)))
 
 (defn generate-grid
   [{:keys [w h]}]
-  (let [column (fn [c] (into [] (for [r (range h)]
-                                 (create-cell :dead c r))))]
+  (let [column (fn [c] (into []
+                            (for [r (range h)]
+                              :dead)))]
     (into [] (map column (range w)))))
 
 (defn on-js-reload []
--- test/gol_cljs/core_tests.cljs
+++ test/gol_cljs/core_tests.cljs
@@ -1,8 +1,7 @@
 (ns ^:figwheel-load gol-cljs.core-tests
   (:require [gol-cljs.core :refer [generate-grid
                                    get-cell
-                                   update-cell
-                                   create-cell]]
+                                   update-cell]]
             [cljs.test :refer-macros [deftest is testing]]))
 
 (deftest grid-generation-tests
@@ -21,26 +20,25 @@
   (testing "get cell"
 
     (testing "get cell in position"
-      (let [grid (generate-grid {:w 10 :h 10})]
-        (is (= [0 0] (:position (get-cell grid 0 0))))
-        (is (= [2 9] (:position (get-cell grid 2 9))))))
+      (let [grid [[:dead :alive]
+                  [:alive :dead]]]
+        (is (= :dead (get-cell grid [0 0])))
+        (is (= :alive (get-cell grid [1 1])))))
 
     (testing "should return nil if out of bounds"
       (let [grid (generate-grid {:w 5 :h 5})]
-        (is (= nil (get-cell grid -1 -1)))
-        (is (= nil (get-cell grid 5 5))))))
+        (is (= nil (get-cell grid [-1 -1])))
+        (is (= nil (get-cell grid [5 5]))))))
 
   (testing "update cell"
 
     (testing "should update the cell"
       (let [grid (generate-grid {:w 2 :h 2})
-            grid' [[{:state :dead :position [0 0]} {:state :dead :position [1 0]}]
-                   [{:state :dead :position [0 1]} {:state :alive :position [1 1]}]]]
-        (is (= grid' (update-cell grid {:state :alive :position [1 1]})))))
+            grid' [[:dead :dead]
+                   [:dead :alive]]]
+        (is (= grid' (update-cell grid [1 1] :alive)))))
 
     (testing "should return the original grid if out of bounds"
-      (let [grid (generate-grid {:w 2 :h 2})
-            cell1 (create-cell :alive -1 -1)
-            cell2 (create-cell :alive 3 3)]
-        (is (= grid (update-cell grid cell1)))
-        (is (= grid (update-cell grid cell2)))))))
+      (let [grid (generate-grid {:w 2 :h 2})]
+        (is (= grid (update-cell grid [-1 -1] :alive)))
+        (is (= grid (update-cell grid [3 3] :alive)))))))
