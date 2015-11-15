ebcdf26b874a20a405716c2a2ee2d85e48aa0b32
Jens Östlund <jostlund@gmail.com>
2015-11-15 23:35:35 +0100

    Added HTML drawing functions

--- src/gol_cljs/core.cljs
+++ src/gol_cljs/core.cljs
@@ -29,6 +29,38 @@
                               :dead)))]
     (into [] (map column (range w)))))
 
+(defn create-element
+  [{:keys [type classes]}]
+  (let [e (.createElement js/document (or type "div"))]
+    (when classes
+      (doseq [c classes]
+        (.. e -classList (add c))))
+    e))
+
+(defn create-cell-element
+  [grid-size state]
+  (create-element {:classes [(str "cell-1-" grid-size) (name state)]}))
+
+(defn create-grid-element
+  [grid]
+  (let [grid-size (count grid)
+        grid-elem (create-element {:classes ["grid"]})
+        columns (for [c grid
+                      :let [column (create-element {:classes [(str "col-1-" grid-size)]})
+                            cells (map (partial create-cell-element grid-size) c)]]
+                  (do (doseq [e cells] (.appendChild column e) cells)
+                      column))]
+    (doseq [c columns] (.appendChild grid-elem c))
+    grid-elem))
+
+(defn draw-grid!
+  [grid]
+  (let [grid-elem (-> (.getElementsByClassName js/document "grid")
+                      (aget 0))]
+    (aset grid-elem "innerHTML" (-> grid
+                                    create-grid-element
+                                    (aget "innerHTML")))))
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
