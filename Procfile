b98a4afd2e752173f44be6d432696505e13140d6
Jens Östlund <jostlund@gmail.com>
2016-03-05 11:33:20 +0100

    Herokified!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/gol-cljs.jar clojure.main -m gol-cljs.server
--- project.clj
+++ project.clj
@@ -6,13 +6,19 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/core.async "0.2.374"]]
+                 [org.clojure/core.async "0.2.374"]
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [bk/ring-gzip "0.1.1"]
+                 [ring.middleware.logger "0.5.0"]
+                 [compojure "1.4.0"]
+                 [environ "1.0.2"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-figwheel "0.5.0-2"]
             [lein-doo "0.1.6-rc.1"]]
 
-  :source-paths ["src"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "resources/test/compiled"
@@ -20,38 +26,20 @@
 
   :min-lein-version "2.5.3"
 
-  :cljsbuild {:builds
-              [{:id "prod"
-                :source-paths ["src"]
-                :figwheel {}
-                :compiler {:output-to "resources/public/js/compiled/gol_cljs.js"
-                           :optimizations :advanced
-                           :pretty-print false}}
+  :uberjar-name "gol-cljs.jar"
 
-               {:id "dev"
-                :source-paths ["src" "test"]
+  :main gol-cljs.server
+
+  :cljsbuild {:builds
+              {:app
+               {:source-paths ["src/cljs" "test"]
                 :figwheel {:on-jsload "gol-cljs.test/run"}
                 :compiler {:main gol-cljs.core
                            :asset-path "js/compiled/out"
                            :output-to "resources/public/js/compiled/gol_cljs.js"
                            :output-dir "resources/public/js/compiled/out"
-                           :source-map true
-                           :source-map-timestamp true}}
+                           :source-map-timestamp true}}}}
 
-               {:id "min"
-                :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/gol_cljs.js"
-                           :main gol-cljs.core
-                           :optimizations :advanced
-                           :pretty-print false}}
-
-               {:id "test"
-                :source-paths ["src" "test"]
-                :compiler {:output-to "resources/test/compiled/tests.js"
-                           :output-dir "resources/test/compiled"
-                           :main gol-cljs.test
-                           :target :nodejs}}]
-}
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
@@ -81,4 +69,34 @@
 
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
+             }
+  :doo [:build "test"]
+
+  :profiles {:dev
+             {:dependencies [[figwheel "0.5.0-6"]
+                             [figwheel-sidecar "0.5.0-6"]
+                             [com.cemerick/piggieback "0.2.1"]
+                             [org.clojure/tools.nrepl "0.2.12"]]
+
+              :plugins [[lein-figwheel "0.5.0-6"]
+                        [lein-doo "0.1.6"]]
+
+              :cljsbuild {:builds
+                          {:test
+                           {:source-paths ["src/cljs" "test"]
+                            :compiler {:output-to "resources/test/compiled/tests.js"
+                                       :output-dir "resources/test/compiled"
+                                       :main gol-cljs.test
+                                       :target :nodejs
+                                       :optimizations :none}}}}}
+             :uberjar
+             {:source-paths ^:replace ["src/clj"]
+              :hooks [leiningen.cljsbuild]
+              :omit-source true
+              :aot :all
+              :cljsbuild {:builds
+                          {:app
+                           {:source-paths ^:replace ["src/cljs"]
+                            :compiler
+                            {:optimizations :advanced
+                             :pretty-print false}}}}}})
